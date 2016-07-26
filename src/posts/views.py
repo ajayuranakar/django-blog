d01f2c977d703e5c1fbf38ff0dc7a5557c4ef14f
ajayuranakar <ajayuranakar@gmail.com>
2016-07-26 20:21:16 +0530

    social_link

--- src/posts/views.py
+++ src/posts/views.py
@@ -4,12 +4,13 @@ from django.http import HttpResponse,HttpResponseRedirect
 from .models import Post
 from .forms import PostForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from urllib import quote_plus
 
 # Create your views here.
 
 #CRUD stuff 
 def post_create(request):
-	form=PostForm(request.POST or None)
+	form=PostForm(request.POST or None,request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		print form.cleaned_data.get("title")
@@ -17,8 +18,8 @@ def post_create(request):
 		#if sucesseed
 		messages.success(request,"Sucessfully created")
 		return HttpResponseRedirect(instance.get_absolute_url())
-	else:
-		messages.error(request,"Problem occured while creation")
+	#else:
+	#	messages.error(request,"Problem occured while creation")
 		
 	#return HttpResponseRedirect(instance.get_absolute_url())
 
@@ -30,19 +31,21 @@ def post_create(request):
 	}
 	return render(request,"post_form.html",context)
 
-def post_detail(request, id=None):#retrive
+def post_detail(request, slug=None):#retrive
 	#instance=Post.objects.get(id=11)
-	instance = get_object_or_404(Post,id=id)
+	instance = get_object_or_404(Post,slug=slug)
+	share_string=quote_plus(instance.content)
 	context = {
 		"title":instance.title,
 		"instance":instance,
+		"share_string":share_string,
 	}
 	return render(request,"post_detail.html",context)
 
 def post_list(request):
 	queryset_list= Post.objects.all()#.order_by("-timestamp")
 	paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
-	page_request_var="abc"
+	page_request_var="page"
 	page = request.GET.get(page_request_var)
 	try:
 		queryset = paginator.page(page)
@@ -75,9 +78,9 @@ def post_list(request):
 
 
 
-def post_update(request,id=None):
-	instance = get_object_or_404(Post,id=id)
-	form=PostForm(request.POST or None, instance=instance)
+def post_update(request,slug=None):
+	instance = get_object_or_404(Post,slug=slug)
+	form=PostForm(request.POST or None,request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
@@ -93,8 +96,8 @@ def post_update(request,id=None):
 	return render(request,"post_form.html",context)
 	#return HttpResponse("<h1>Hello update</h1>")
 
-def post_delete(request, id=None):
-	instance = get_object_or_404(Post,id=id)
+def post_delete(request, slug=None):
+	instance = get_object_or_404(Post,slug=slug)
 	instance.delete()
 	messages.success(request,"Sucessfully deleted")
 	#message sucess
